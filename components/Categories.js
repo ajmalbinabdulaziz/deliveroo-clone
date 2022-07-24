4083b8a567750cad74e9551f3a0f67d8a07a9d83
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-07-24 12:13:40 +0530

    sanity schemas redefined

---
+++ components/Categories.js
@@ -0,0 +1,40 @@
+import { ScrollView, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import CategoryCard from './CategoryCard'
+
+const Categories = () => {
+    return (
+      <ScrollView
+        contentContainerStyle={{
+            paddingHorizontal: 15,
+            paddingTop: 10,
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        <CategoryCard 
+            imgUrl="https://links.papareact.com/gn7"
+            title="Testing 1" 
+        />
+        <CategoryCard 
+            imgUrl="https://links.papareact.com/gn7"
+            title="Testing 2" 
+        />
+        <CategoryCard 
+            imgUrl="https://links.papareact.com/gn7"
+            title="Testing 3" 
+        />
+        <CategoryCard 
+            imgUrl="https://links.papareact.com/gn7"
+            title="Testing 4" 
+        />
+        <CategoryCard 
+            imgUrl="https://links.papareact.com/gn7"
+            title="Testing 5" 
+        />
+
+      </ScrollView>
+    )
+}
+
+export default Categories
