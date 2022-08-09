4d20a10635da3458d704b99272af99bc6eae0a2f
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2022-08-10 00:11:06 +0530

    partially completed PreparingOrderScreen, video-3.39

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import { Provider } from 'react-redux'
 import { store } from './store';
 import RestaurantScreen from './screens/RestaurantScreen';
 import BasketScreen from './screens/BasketScreen';
+import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -22,6 +24,12 @@ export default function App() {
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
             <Stack.Screen name="Basket" component={BasketScreen} 
               options={{ presentation: "modal", headerShown: false }}  
+            />
+            <Stack.Screen name="PreparingOrderScreen" component={PreparingOrderScreen} 
+              options={{ presentation: "fullScreenModal", headerShown: false }}  
+            />
+            <Stack.Screen name="DeliveryScreen" component={DeliveryScreen} 
+              options={{ presentation: "fullScreenModal", headerShown: false }}  
             />
           </Stack.Navigator>
         </TailwindProvider>
